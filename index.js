35e62ddcd75fd96708f16b00f45dcaddc157f6c1
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-06-27 15:44:21 -0700

    commit tree fac4652ce4481b420b9d24f7878e243d7554bed7

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result;
+  
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  }
+  
+    else if (someValue >= 400 && someValue <= 2000 ) {
+    result = "That will be twenty bucks."
+  }
+    else if (someValue >= 2000 && someValue <= 2500) {
+      result = "I will gladly take your thirty bucks." 
+    }
+      else {
+        result = "No can do."
+      }
+    
+    return result;
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(mainCity){
+  return (mainCity === "NYC" ? "Ok, sounds good." : "No go.");
 }
+ternaryCheckCity("NYC");
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let niceness;
+
+  switch (tip) {
+    case "generous":
+      niceness = "Thank you so much.";
+    break;
+    case "not as generous":
+      niceness = "Thank you.";
+    break;
+    default: 
+      niceness = "Bye."
+    break;
+  }
+return niceness;
+  
 }
